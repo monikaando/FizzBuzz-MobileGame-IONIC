2016ce20b9f7b904b213e58b4dbee5a88ec4189e
Monika Swidzinska <monikaswidzinska@Monikas-MacBook-Pro.local>
2020-08-28 19:43:25 +0200

    3lives

--- src/app/game/game.page.ts
+++ src/app/game/game.page.ts
@@ -9,102 +9,135 @@ import {Input} from "../models/input";
 import {Answer} from "../models/answer";
 import {Results} from "../models/results";
 
-
-
 @Component({
-  selector: 'app-game',
-  templateUrl: './game.page.html',
-  styleUrls: ['./game.page.scss'],
+    selector: 'app-game',
+    templateUrl: './game.page.html',
+    styleUrls: ['./game.page.scss'],
 })
 export class GamePage implements OnInit {
-  title = 'FizzBuzzGame';
-  score$ = 0;
-  numbers$: Observable<number>;
-  answers$: Observable<Answer[]>;
-  numbersSub$: Subscription;
-  game$: Subscription;
-  public onStartClick = new Subject<boolean>();
-  constructor(protected fizzBuzzService: FizzBuzzService) { }
-  @ViewChild('numberButton', {static: true, read: ElementRef}) numberButton: ElementRef;
-  @ViewChild('fizzButton', {static: true, read: ElementRef}) fizzButton: ElementRef;
-  @ViewChild('buzzButton', {static: true, read: ElementRef}) buzzButton: ElementRef;
-  @ViewChild('fizzBuzzButton', {static: true, read: ElementRef}) fizzBuzzButton: ElementRef;
+    title = 'FizzyBuzzGame';
+    score$ = 0;
+    life$ = 3;
+    numbers$: Observable<number>;
+    answers$: Observable<Answer[]>;
+    numbersSub$: Subscription;
+    game$: Subscription;
+    wrongAnswer$: Observable<number>;
+    wrongAnswerSub$: Subscription;
+    public onStartClick = new Subject<boolean>();
 
-  ngOnInit(): void {
-    this.onStartClick.subscribe((response) => {
-      this.playGame();
-      this.numbers$ = this.fizzBuzzService.numbers$;
-      this.numbersSub$ = this.numbers$.subscribe(val => {
-        return val
-      });
-    });
-  }
+    constructor(protected fizzBuzzService: FizzBuzzService) {
+    }
 
-  playGame() {
-    const numberBtn = fromEvent(this.numberButton.nativeElement, 'click');
-    const fizzBtn = fromEvent(this.fizzButton.nativeElement, 'click');
-    const buzzBtn = fromEvent(this.buzzButton.nativeElement, 'click');
-    const fizzBuzzBtn = fromEvent(this.fizzBuzzButton.nativeElement, 'click');
+    @ViewChild('numberButton', {static: true, read: ElementRef}) numberButton: ElementRef;
+    @ViewChild('fizzButton', {static: true, read: ElementRef}) fizzButton: ElementRef;
+    @ViewChild('buzzButton', {static: true, read: ElementRef}) buzzButton: ElementRef;
+    @ViewChild('fizzBuzzButton', {static: true, read: ElementRef}) fizzBuzzButton: ElementRef;
 
-    const ChoiceArray = (): Observable<Input> =>
-        merge<Choice>(
-            numberBtn.pipe(mapTo('Number')),
-            fizzBtn.pipe(mapTo('Fizz')),
-            buzzBtn.pipe(mapTo('Buzz')),
-            fizzBuzzBtn.pipe(mapTo('FizzBuzz')),
-            this.fizzBuzzService.numbers$.pipe(mapTo('-')),
-        ).pipe<Input>(
-            first(null, null),
+
+    ngOnInit(): void {
+        this.onStartClick.subscribe((response) => {
+            this.playGame();
+            this.numbers$ = this.fizzBuzzService.numbers$;
+            this.numbersSub$ = this.numbers$.subscribe(val => {
+                return val
+            });
+        });
+    }
+
+    playGame() {
+        const numberBtn = fromEvent(this.numberButton.nativeElement, 'click');
+        const fizzBtn = fromEvent(this.fizzButton.nativeElement, 'click');
+        const buzzBtn = fromEvent(this.buzzButton.nativeElement, 'click');
+        const fizzBuzzBtn = fromEvent(this.fizzBuzzButton.nativeElement, 'click');
+
+        const ChoiceArray = (): Observable<Input> =>
+            merge<Choice>(
+                numberBtn.pipe(mapTo('Number')),
+                fizzBtn.pipe(mapTo('Fizz')),
+                buzzBtn.pipe(mapTo('Buzz')),
+                fizzBuzzBtn.pipe(mapTo('FizzBuzz')),
+                this.fizzBuzzService.numbers$.pipe(mapTo('-')),
+            ).pipe<Input>(
+                first(null, null),
+            );
+
+        const game$ = zip<[number, Choice, Input, number[]]>(
+            this.fizzBuzzService.numbers$,
+            this.fizzBuzzService.fizzBuzz$,
+            this.fizzBuzzService.numbers$
+                .pipe(
+                    delay(1),
+                    switchMap(ChoiceArray)
+                )
+        ).pipe(
+            share()
         );
 
-    const game$ = zip<[number, Choice, Input, number[]]>(
-        this.fizzBuzzService.numbers$,
-        this.fizzBuzzService.fizzBuzz$,
-        this.fizzBuzzService.numbers$
-            .pipe(
-                delay(1),
-                switchMap(ChoiceArray)
-            )
-    ).pipe(
-        share()
-    );
+        const score$ = game$.pipe
+        (scan((score, [numb, correctAnswer, userAnswer]) =>
+            userAnswer && ((isNumeric(correctAnswer) && userAnswer === 'Number') ||
+                (correctAnswer === userAnswer)) ? score + 1 : score - 1, 0)
+        );
+        this.answers$ = zip(game$, score$).pipe
+        (scan<[[number, Choice, Input, number[]], number], Answer[]>((answer, [[numb, correct, user], points]) =>
+            concat([{numb, correct, user, points}], answer), []));
 
-    const score$ = game$.pipe
-    (scan((score, [numb, correctAnswer, userAnswer]) =>
-        userAnswer && ((isNumeric(correctAnswer) && userAnswer === 'Number') ||
-            (correctAnswer === userAnswer)) ? score + 1 : score - 1, 0)
-    );
-
-    this.answers$ = zip(game$, score$).pipe
-    (scan<[[number, Choice, Input, number[]], number], Answer[]>((answer, [[numb, correct, user], points]) =>
-        concat([{numb, correct, user, points}], answer), []));
-
-    const fizzBuzzGame$ = zip<[number, Answer[]]>(score$, this.answers$).pipe(
-        map(([score, answer]) => ({score, answer} as Results)
+        const correctAnswer$: Observable<boolean> = game$.pipe(
+            map(([numb, gameAnswer, userAnswer]) => {
+                return userAnswer && ((userAnswer === gameAnswer) ||
+                    (userAnswer == 'Number' && isNumeric(gameAnswer)));
+            }));
+        this.wrongAnswer$ = correctAnswer$.pipe(
+            scan((wrong, correct) => {
+                if (!correct) {
+                    wrong++;
+                }
+                return wrong;
+            }, 0)
         )
-    );
+        this.wrongAnswerSub$ = this.wrongAnswer$
+            .subscribe((wrongAnswer) => {
+                if (wrongAnswer >= this.life$ || this.score$ < 0) {
+                    this.reset();
+                }
+            })
+        const fizzBuzzGame$ = zip<[number, Answer[]]>(score$, this.answers$).pipe(
+            map(([score, answer]) => ({score, answer} as Results)
+            )
+        );
 
-    this.game$ = fizzBuzzGame$.subscribe((results: Results) => {
-      this.score$ = results.score;
-      this.score$ === -1 ? this.reset() : null;
-    });
-  }
+        this.game$ = fizzBuzzGame$.subscribe((results: Results) => {
+            this.score$ = results.score;
+        });
 
-  reset(): void {
-    alert('Game Over!');
-    this.stopGame();
-  }
-  stopGame(): void{
-    this.score$ = 0;
-    this.numbersSub$.unsubscribe();
-    this.game$.unsubscribe();
-    this.numbers$ = null;
-    this.answers$ = null;
-  }
+    }
 
-  correctAnswer(userAnswer, gameAnswer):boolean {
-    return userAnswer && ((userAnswer === gameAnswer) ||
-        (userAnswer == 'Number' && isNumeric(gameAnswer)))
-  }
+    reset(): void {
+        if(this.score$===-1){
+            alert(`Game Over!, You\'ve got 0 points`);
 
+        }
+        else {
+            alert(`Game Over!, You\'ve got ${this.score$} points`);
+        }
+
+        this.stopGame();
+    }
+
+    stopGame(): void {
+        this.score$ = 0;
+        this.life$ = 3;
+        this.numbers$ = null;
+        this.answers$ = null;
+        this.numbersSub$.unsubscribe();
+        this.game$.unsubscribe();
+        this.wrongAnswer$ = null;
+        this.wrongAnswerSub$.unsubscribe();
+    }
+
+    correctAnswer(userAnswer, gameAnswer): boolean {
+        return userAnswer && ((userAnswer === gameAnswer) ||
+            (userAnswer == 'Number' && isNumeric(gameAnswer)));
+    }
 }
